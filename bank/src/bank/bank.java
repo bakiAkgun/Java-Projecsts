ad753e9a5b4e3ddc87f8705ce7ca17c094738581
bakiAkgun <116957673+bakiAkgun@users.noreply.github.com>
2022-10-31 17:22:30 +0300

    ATM machine

--- bank/src/bank/bank.java
+++ bank/src/bank/bank.java
@@ -4,22 +4,11 @@ import java.util.Scanner;
 
 public class bank {
 	
-		 static void first() {
-			 Scanner scan= new Scanner(System.in);
-				
-				int[] numbers1= new int[10];
-				int[] numbers= {0,1,2,3,4,5,6,7,8,9};
-				numbers[0]=100;
-				numbers[1]=100;
-				numbers[2]=100;
-				numbers[3]=100;
-				numbers[4]=100;
-				numbers[5]=100;
-				numbers[6]=100;
-				numbers[7]=100;
-				numbers[8]=100;
-				numbers[9]=100;
-		 }
+		static Scanner scan= new Scanner(System.in);
+		static int[] numbers1= {100,100,100,100,100,100,100,100,100,100};
+		static int[] identify= {0,1,2,3,4,5,6,7,8,9};
+		 static int n;
+		 
 			static void myMethod() {
 				System.out.println("Please choose your what you want operation ");
 				System.out.println("1-Check balance");
@@ -32,22 +21,11 @@ public class bank {
 				  
 			}
 			static void again(){
-				 Scanner scan= new Scanner(System.in);
 					
-					int[] numbers1= new int[10];
-					int[] numbers= {0,1,2,3,4,5,6,7,8,9};
-					numbers[0]=100;
-					numbers[1]=100;
-					numbers[2]=100;
-					numbers[3]=100;
-					numbers[4]=100;
-					numbers[5]=100;
-					numbers[6]=100;
-					numbers[7]=100;
-					numbers[8]=100;
-					numbers[9]=100;
-				first();
-				System.out.println("Please enter same id again");
+					
+				System.out.println("Welcome this ATM machine. Please insert your card");
+				System.out.println("please enter an id:");
+				
 				 int n= scan.nextInt();
 					
 				myMethod();
@@ -58,22 +36,21 @@ public class bank {
 
 		case 1:
 		System.out.println("Check Balance");
-		System.out.println("Your balance is: "+numbers[n]);
+		System.out.println("Your balance is: "+numbers1[n]);
 		break;
 		case 2:
 		System.out.println("Withdraw");
 		System.out.println("Enter an amount to withdraw: ");
 		int withdraw=scan.nextInt();
-		System.out.println(withdraw);
-
+		numbers1[n] -=withdraw;
 		
-		if(withdraw<=numbers[n]) {
+		if(withdraw<=numbers1[n]) {
 			System.out.println("your money is being preparing... your money is ready");
 			
 		}
 		else {
 			System.out.println("Withdraw amount is too large.you are redirected to deposit menu ");
-			System.out.println("You should insert remainder money: "+(withdraw-numbers[n]));
+			System.out.println("You should insert remainder money: "+(withdraw-numbers1[n]));
 		}
 		break;
 		case 3:
@@ -81,34 +58,32 @@ public class bank {
 		int deposit=scan.nextInt();
 
 		System.out.println("your process is completed. added:  "+deposit);
+		numbers1[n] +=deposit;
 		break;
+		
 		case 4:
 		System.out.println("please take back your card.");
+	   System.exit(11);
 		break;
 
 		default:
 			System.out.println("you entered invalid number of operation");
 		}
 		again();
+					}
 
-
-			}
-			public static void main(String[] args) {
-       Scanner scan= new Scanner(System.in);
 			
+			public static void main(String[] args) {
+      
+		
 				
-
-				System.out.println("Welcome this ATM machine. Please insert your card");
-				System.out.println("please enter an id:");
-				
-				 int n= scan.nextInt();
 				if(n<10) {
 		again();
 
 				}
 				else{
 					System.out.println("you should enter valid id.");
-				}	  
+				}	 
 	
 
    
